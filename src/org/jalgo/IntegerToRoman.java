655fc6ce3166c822e2cd0629c6bec4e2f1df0ac2
rajhub <raj.pitchaimani@gmail.com>
2015-03-02 16:53:45 -0800

    add Roman to integer and vice versa

---
+++ src/org/jalgo/IntegerToRoman.java
@@ -0,0 +1,26 @@
+package org.jalgo;
+
+public class IntegerToRoman {
+	static int[] NUMBERS = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900,
+			1000 };
+	static String[] ROMAN_LETTERS = { "I", "IV", "V", "IX", "X", "XL", "L",
+			"XC", "C", "CD", "D", "CM", "M" };
+
+	public static void main(String[] args) {
+		System.out.println(toNum("MCMLXXXIV"));
+	}
+
+	private static int toNum(String roman) {
+		int offset = 0;
+		int val = 0;
+		while (offset < roman.length()) {
+			for (int i = ROMAN_LETTERS.length - 1; i >= 0; i--) {
+				if (roman.startsWith(ROMAN_LETTERS[i], offset)) {
+					val += NUMBERS[i];
+					offset += ROMAN_LETTERS[i].length();
+				}
+			}
+		}
+		return val;
+	}
+}
---
+++ src/org/jalgo/RomanToInteger.java
@@ -0,0 +1,24 @@
+package org.jalgo;
+
+public class RomanToInteger {
+	static int[] NUMBERS = { 1, 4, 5, 9, 10, 40, 50, 90, 100, 400, 500, 900,
+			1000 };
+	static String[] ROMAN_LETTERS = { "I", "IV", "V", "IX", "X", "XL", "L",
+			"XC", "C", "CD", "D", "CM", "M" };
+
+	public static void main(String[] args) {
+		System.out.println(toRoman(1984));
+	}
+
+	private static String toRoman(int number) {
+		
+		String val = "";
+		for (int i = NUMBERS.length - 1; i >= 0; i--) {
+			while (number >= NUMBERS[i]) {
+				val += ROMAN_LETTERS[i];
+				number -= NUMBERS[i];
+			}
+		}
+		return val;
+	}
+}
