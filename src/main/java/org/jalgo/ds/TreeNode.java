586a3bc4d0faaa07d7e4997000fdd6cf0785a75a
rajhub <raj.pitchaimani@gmail.com>
2015-04-02 15:38:37 -0700

    update

--- src/main/java/org/jalgo/ds/TreeNode.java
+++ src/main/java/org/jalgo/ds/TreeNode.java
@@ -4,4 +4,8 @@ public class TreeNode {
 	Object value;
 	TreeNode left;
     TreeNode right;
+
+    public TreeNode(Object value) {
+        this.value = value;
+    }
 }
--- src/main/java/org/jalgo/ds/TreeTraverse.java
+++ src/main/java/org/jalgo/ds/TreeTraverse.java
@@ -7,6 +7,25 @@ import java.util.concurrent.ConcurrentLinkedQueue;
 
 public class TreeTraverse {
 
+    public static void main(String args[]) {
+        TreeNode t1 = new TreeNode(1);
+        TreeNode t2 = new TreeNode(2);
+        TreeNode t3 = new TreeNode(3);
+        TreeNode t4 = new TreeNode(4);
+        TreeNode t5 = new TreeNode(5);
+        TreeNode t6 = new TreeNode(6);
+        TreeNode t7 = new TreeNode(7);
+
+        t4.left = t2;
+        t2.left = t1;
+        t2.right = t3;
+        t4.right = t6;
+        t6.left = t5;
+        t6.right = t7;
+
+        printTree(t4);
+    }
+
     public static int height(TreeNode root) {
         if (root == null)
             return -1;
@@ -15,17 +34,18 @@ public class TreeTraverse {
     }
 
     // breadth first traversal
-    public static void levelOrder(TreeNode root) {
+    public static List levelOrder(TreeNode root) {
         java.util.Queue<TreeNode> queue = new ConcurrentLinkedQueue<TreeNode>();
+        ArrayList<Object> list = new ArrayList<Object>();
 
         if (root == null)
-            return;
+            return list;
 
         queue.add(root);
 
         while(queue.peek()!= null) {
             TreeNode node = queue.poll();
-            System.out.println(node.value);
+            list.add(node.value);
             if (node.left != null) {
                 queue.add(node.left);
             }
@@ -34,7 +54,35 @@ public class TreeTraverse {
             }
         }
 
-        return;
+        return list;
+    }
+
+    // Print like a tree
+    public static void printTree(TreeNode root) {
+        java.util.Queue<TreeNode> queue = new ConcurrentLinkedQueue<TreeNode>();
+
+        if (root == null)
+            return;
+
+        queue.add(root);
+        queue.add(new TreeNode(null));
+
+        while(queue.peek()!= null) {
+            TreeNode node = queue.poll();
+            if (node.value != null) {
+                System.out.print(node.value + " ");
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            } else {
+                System.out.println();
+                if (queue.isEmpty()) break;
+                queue.add(new TreeNode(null));
+            }
+        }
     }
 
     // depth first - data, left, right
@@ -70,12 +118,16 @@ public class TreeTraverse {
             return list;
 
         Stack<TreeNode> stack = new Stack<TreeNode>();
-
         TreeNode p = root;
 
         while(!stack.empty() || p != null) {
             if (p != null) {
                 stack.push(p);
+                p = p.left;
+            } else {
+                TreeNode node = stack.pop();
+                list.add(node.value);
+                p = node.right;
             }
         }
 
