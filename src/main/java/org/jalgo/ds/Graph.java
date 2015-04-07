91127b4fd8270450b9c7f2aa731368fcc5f39ec2
rajhub <raj.pitchaimani@gmail.com>
2015-04-07 13:20:24 -0700

    add graph

---
+++ src/main/java/org/jalgo/ds/Graph.java
@@ -0,0 +1,26 @@
+package org.jalgo.ds;
+
+import java.util.*;
+
+public class Graph {
+    private Map<String, List<String>> edges = new HashMap<String, List<String>>();
+
+    public void addEdge(String src, String dest) {
+        List<String> srcNeighbors = this.edges.get(src);
+        if (srcNeighbors == null) {
+            this.edges.put(src,
+                    srcNeighbors = new ArrayList<String>()
+            );
+        }
+        srcNeighbors.add(dest);
+    }
+
+    public Iterable<String> getNeighbors(String vertex) {
+        List<String> neighbors = this.edges.get(vertex);
+        if (neighbors == null) {
+            return Collections.emptyList();
+        } else {
+            return Collections.unmodifiableList(neighbors);
+        }
+    }
+}
