4c96d13937a7bcd24c17b8ae6ef1ed7ece008fc8
rajhub <raj.pitchaimani@gmail.com>
2015-04-09 17:26:46 -0700

    add Linked list palindrome

---
+++ src/main/java/org/jalgo/ds/linkedlist/ListPalindrome.java
@@ -0,0 +1,38 @@
+package org.jalgo.ds.linkedlist;
+
+import java.util.Stack;
+
+public class ListPalindrome {
+    public static boolean isPalindrome(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+
+        Stack<Integer> stack = new Stack<Integer>();
+
+        /* Push elements from first half of linked list onto stack. When
+         * fast runner (which is moving at 2x speed) reaches the end of
+         * the linked list, then we know we're at the middle
+         */
+        while (fast != null && fast.next != null) {
+            stack.push((Integer) slow.value);
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+         /* Has odd number of elements, so skip the middle element */
+        if (fast != null) {
+            slow = slow.next;
+        }
+        while (slow != null) {
+            int top = stack.pop();
+
+                /* If values are different, then it's not a palindrome */
+            if (top != slow.value) {
+                return false;
+            }
+            slow = slow.next;
+        }
+        return true;
+    }
+
+}
